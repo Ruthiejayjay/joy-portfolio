724ebb9f67ffb8174f628f501e820fa49d97323b
Ruth James <jamesruth449@gmail.com>
2025-05-18 15:49:31 +0100

    hero section completed

--- src/components/Hero/Index.tsx
+++ src/components/Hero/Index.tsx
@@ -1,18 +1,26 @@
 import clsx from "clsx";
 import styles from "./styles.module.scss";
+import { Carlito } from "next/font/google";
+
+const carlitoFont = Carlito({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 export default function Hero() {
   return (
     <div className={clsx(styles.hero)}>
       <div className={clsx(styles.container, "container")}>
         <div className={styles.heading}>
-          <div className={styles.title}>Hello, I am Joy!</div>
-          <p className={styles.desc}>
+          <h2 className={styles.title}>
+            Hello,<span> I am Joy!</span>
+          </h2>
+          <p className={clsx(styles.desc, carlitoFont.className)}>
             I believe brevity is key. My print and digital designs are all about
             minimalism and elegance.
           </p>
 
-          <button>Hire me</button>
+          <button className={styles.hireBtn}>Hire me</button>
         </div>
       </div>
     </div>
